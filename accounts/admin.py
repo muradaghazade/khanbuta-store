3074204b3436269ec8bbe62d9e844101a5ffd5ce
Murad Aghazada <tariyelagazade@Murads-MacBook-Pro3.local>
2022-03-24 16:55:05 +0400

    m

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,4 +1,4 @@
 from django.contrib import admin
 from accounts.models import *
 
-admin.site.register([User, OTPCode, City, Region, Avenue, Street, SocialIcon, SocialMedia,])
+admin.site.register([User, OTPCode, City, Region, Avenue, Street, SocialIcon, SocialMedia, UserCategory])
