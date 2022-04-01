886519f7f086676aa8bfe8d39bd4de2cc4e39c99
Murad Aghazada <tariyelagazade@Murads-MacBook-Pro3.local>
2022-04-01 19:11:58 +0400

    m

--- products/admin.py
+++ products/admin.py
@@ -1,4 +1,14 @@
 from django.contrib import admin
 from .models import *
 
-admin.site.register([Logo, Category, HeaderText, SubCategory, SubSubCategory, Filter, CategoryLine, Slider, Benefit, DisplayedCategory, Product, Partner, Image, FilterValue, Tag, AboutUs, AboutUsService, AboutUsCarousel, FAQCategory, FAQ, Rating, DiscountProduct, Wishlist, ProductVersion, Cart, CategoryBanner, Number, StoreOrder, Order, SocialLink, CategoryReklam])
+admin.site.register([Logo, Category, HeaderText, SubCategory, SubSubCategory, Filter, CategoryLine, Slider, Benefit, DisplayedCategory, Partner, Image, FilterValue, Tag, AboutUs, AboutUsService, AboutUsCarousel, FAQCategory, FAQ, Rating, DiscountProduct, Wishlist, ProductVersion, Cart, CategoryBanner, Number, StoreOrder, Order, SocialLink, CategoryReklam])
+
+class ProductImageInline(admin.TabularInline):
+    model = Image
+
+class ProductAdmin(admin.ModelAdmin):
+    inlines = [ProductImageInline]
+    raw_id_fields = ['user']
+
+
+admin.site.register(Product, ProductAdmin)
