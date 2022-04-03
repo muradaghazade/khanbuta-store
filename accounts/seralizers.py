0b83b2c73141c9bb1e2c40e2931d69db663e84c7
Murad Aghazada <tariyelagazade@Murads-MacBook-Pro3.local>
2022-04-04 02:35:13 +0400

    m

--- accounts/seralizers.py
+++ accounts/seralizers.py
@@ -69,7 +69,6 @@ class SocialMediaSerializer(serializers.ModelSerializer):
 
 
 class SocialIconCreateSerializer(serializers.ModelSerializer):
-    social_media = SocialMediaSerializer(required=False)
     class Meta:
         model = SocialIcon
         fields = ('id', 'url', 'social_media', 'user', 'created_at', 'updated_at')
