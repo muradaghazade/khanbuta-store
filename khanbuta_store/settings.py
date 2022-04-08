2149a6edf1dec31471bd12344ce08814461db6ec
Murad Aghazada <tariyelagazade@Murads-MacBook-Pro3.local>
2022-04-08 22:03:37 +0400

    m

--- khanbuta_store/settings.py
+++ khanbuta_store/settings.py
@@ -155,7 +155,7 @@ STATIC_URL = '/static/'
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
-MEDIA_URL = 'https://api.ipekyolu.az/media/'
+MEDIA_URL = 'media/'
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
 
