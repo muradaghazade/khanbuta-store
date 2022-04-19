9987bca5dd00e4704735f7f19869e104cce0bcd7
Murad Aghazada <tariyelagazade@Murads-MacBook-Pro3.local>
2022-04-19 15:09:38 +0400

    m

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,4 +1,17 @@
 from django.contrib import admin
 from accounts.models import *
 
-admin.site.register([User, OTPCode, City, Region, Avenue, Street, SocialIcon, SocialMedia, UserCategory])
+admin.site.register([OTPCode, City, Region, Avenue, Street, SocialIcon, SocialMedia, UserCategory])
+
+class UserAdmin(admin.ModelAdmin):
+    list_display = ('number', 'is_vendor')
+    # readonly_fields = ('membership_id',)
+    # ordering = ('-id',)
+    search_fields = ('email', 'number', 'name', 'first_name', 'last_name')
+
+admin.site.register(User, UserAdmin)
+
+
+admin.site.site_header = "Ipekyolu"
+admin.site.index_title = "Ipekyolu"
+admin.site.site_title = "Ipekyolu Administration"
